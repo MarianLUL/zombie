f1cd5839c887d43c4d65a843f4784655311e5506
Marian <148313996+MarianLUL@users.noreply.github.com>
2025-01-21 12:28:37 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let bullets = [];
 let score = 0;
 let canvas;
 let lastShotTime = 0;
-const shotDelay = 0; // 0.3 second delay in milliseconds
+const shotDelay = 1; // 0.3 second delay in milliseconds
 
 
 function setup() {
